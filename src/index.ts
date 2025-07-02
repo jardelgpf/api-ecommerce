8cf364a45b685b079f048f4a1cc1f2d9beedb107
Jardel Thom <jardel_thom@hotmail.com>
2025-07-02 10:05:29 -0300

    att

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,12 @@ import express from "express";
 
 const app = express()
 app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send("servidor rodando")
+})
+
+app.listen(3000, () => {
+    console.log("servidor rodando");
+    
+})
