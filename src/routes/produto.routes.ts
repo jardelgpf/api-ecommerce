6b266f50ddcfde5e7fdcb7678b38aac2e92fb3f5
Jardel Thom <jardel_thom@hotmail.com>
2025-07-14 09:20:37 -0300

    deploy final

--- src/routes/produto.routes.ts
+++ src/routes/produto.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { ProdutoController } from "../controllers/ProdutoController";
+import { authJwt } from "../middleware/authJwt";
 
 const routes = Router()
 
-routes.get('/', ProdutoController.listar)
-routes.get('/:id',ProdutoController.buscar)
-routes.post('/', ProdutoController.criar)
-routes.put('/:id', ProdutoController.atualizar)
-routes.delete('/:id', ProdutoController.deletar)
+routes.get('/',authJwt, ProdutoController.listar)
+routes.get('/:id',authJwt,ProdutoController.buscar)
+routes.post('/',authJwt, ProdutoController.criar)
+routes.put('/:id',authJwt, ProdutoController.atualizar)
+routes.delete('/:id',authJwt, ProdutoController.deletar)
 
 export default routes
